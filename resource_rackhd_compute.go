f515e8ea7ac560cd6dba31e0a6fb89c18633c85b
John Frey <john.frey@emc.com>
2015-12-10 14:26:21 -0800

    update compute resource to deploy centos 6.5

--- resource_rackhd_compute.go
+++ resource_rackhd_compute.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/hashicorp/terraform/helper/schema"
 	"github.com/jfrey/go-rackhd"
 )
@@ -13,80 +15,86 @@ func resourceRackHDCompute() *schema.Resource {
 		Delete: resourceRackHDComputeDelete,
 
 		Schema: map[string]*schema.Schema{
-			"workflow": &schema.Schema{
+			// Optional specification for a Node ID directly.
+			"node": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+
+			// Compute Host Configuration
+			"os": &schema.Schema{
 				Type:     schema.TypeString,
 				Required: true,
 			},
-			"sku": &schema.Schema{
+			"interface": &schema.Schema{
 				Type:     schema.TypeString,
-				Computed: true,
+				Required: true,
 			},
-			"type": &schema.Schema{
+			"ip": &schema.Schema{
 				Type:     schema.TypeString,
-				Computed: true,
+				Required: true,
 			},
-			"ipaddresses": &schema.Schema{
-				Type:     schema.TypeList,
-				Computed: true,
-				Elem: &schema.Schema{
-					Type: schema.TypeString,
-				},
+			"gateway": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"subnet": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"dns_server": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"domain": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"hostname": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"root_password": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"user": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"password": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"uid": &schema.Schema{
+				Type:     schema.TypeInt,
+				Required: true,
 			},
 		},
 	}
 }
 
 func resourceRackHDComputeCreate(d *schema.ResourceData, meta interface{}) error {
-	node, err := resourceCheckout(d, meta)
+	resourceID, err := resourceIdentify(d, meta)
 	if err != nil {
 		return err
 	}
 
-	d.SetId(node.ID)
+	client := meta.(*rackhd.Client)
 
-	// TODO: Execute Workflow
+	body, err := composeWorkflowOptions(d)
+
+	err = client.NodeRunWorkflow(client.NodePostWorkflow, client.GetWorkflowByID, resourceID, *body)
+	if err != nil {
+		return err
+	}
+
+	d.SetId(resourceID)
 
 	return resourceRackHDComputeRead(d, meta)
 }
 
 func resourceRackHDComputeRead(d *schema.ResourceData, meta interface{}) error {
-	client := meta.(*rackhd.Client)
-
-	node, err := client.GetNode(d.Id())
-	if err != nil {
-		return err
-	}
-
-	// Set computed properties from the Node document.
-	d.Set("type", node.Type)
-
-	// Get the friendly entry for the Sku if possible.
-	if node.Sku != "" {
-		sku, err := client.GetSku(node.Sku)
-		if err == nil {
-			d.Set("sku", sku.Name)
-		} else {
-			d.Set("sku", node.Sku)
-		}
-	}
-
-	// Get the Lookup entries for the node for a computed IP Addresses property.
-	lookups, err := client.QueryLookups(node.ID)
-	if err != nil {
-		return err
-	}
-
-	// Assign all valid IP Addresses
-	var ipaddresses []string
-	for _, lookup := range lookups {
-		if lookup.IPAddress != "" {
-			ipaddresses = append(ipaddresses, lookup.IPAddress)
-		}
-	}
-
-	// Set the computed property for IP Addresses.
-	d.Set("ipaddresses", ipaddresses)
-
 	return nil
 }
 
@@ -96,4 +104,63 @@ func resourceRackHDComputeUpdate(d *schema.ResourceData, meta interface{}) error
 
 func resourceRackHDComputeDelete(d *schema.ResourceData, meta interface{}) error {
 	return resourceCheckin(d, meta)
+}
+
+func composeWorkflowOptions(d *schema.ResourceData) (*rackhd.WorkflowRequest, error) {
+	if v, ok := d.GetOk("os"); ok {
+		switch v.(string) {
+		case "centos-6.5":
+			return composeCentos65Options(d)
+		}
+
+		return nil, fmt.Errorf("Unsupported OS Requested: %s\n", v.(string))
+	}
+
+	return nil, fmt.Errorf("Required paramter 'os' not specified.")
+}
+
+func composeCentos65Options(d *schema.ResourceData) (*rackhd.WorkflowRequest, error) {
+	user := rackhd.User{
+		d.Get("user").(string),
+		d.Get("password").(string),
+		d.Get("uid").(int),
+	}
+	users := rackhd.Users{user}
+	net := rackhd.NetworkDevice{
+		d.Get("interface").(string),
+		rackhd.Ipv4{
+			d.Get("gateway").(string),
+			d.Get("ip").(string),
+			d.Get("subnet").(string),
+		},
+	}
+	nets := rackhd.NetworkDevices{net}
+	dns := []string{
+		d.Get("dns_server").(string),
+	}
+	domain := d.Get("domain").(string)
+	hostname := d.Get("hostname").(string)
+	rootpass := d.Get("root_password").(string)
+	version := "6.5"
+	install := rackhd.InstallOsStruct{
+		dns,
+		domain,
+		hostname,
+		rootpass,
+		version,
+		nets,
+		users,
+		"",
+		"",
+		"",
+	}
+	defaults := rackhd.WorkflowOptionDefaults{
+		InstallOs: install,
+	}
+	body := rackhd.WorkflowRequest{
+		Name:    "Graph.InstallCentOS",
+		Options: defaults,
+	}
+
+	return &body, nil
 }
